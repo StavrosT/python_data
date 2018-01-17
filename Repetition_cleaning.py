6a674b31e03fc10606d9531c4d1c98bdc02dc68f
Stavros Trakoshis <stauros.trak@gmail.com>
2018-01-17 02:02:47 +0200

    Update Repetition_cleaning.py
    Now it removes empty columns, deletes spaces from column names and deletes rows that are not of interest

--- Repetition_cleaning.py
+++ Repetition_cleaning.py
@@ -1,11 +1,12 @@
 
 #!/bin/bash/env python
-# PREPROCESSING STEPS:
-# 1) Find and Replace (Delete) all spaces in datasets .csv
-# 2) delete all empty columns
-##########
-# This script deletes rows that are not of interest 
 
+##########
+# This script 
+# removes spaces from column names
+# removes empty columns
+# deletes rows that are not of interest
+##########
 
 
 #Import modules
@@ -13,18 +14,28 @@ import pandas as pd
 
 
 # Sepcify csv file name to import
-DATA = '8100_ICUY_20160216_readable.csv'
+DATA = '8100_STAI_20160216_readable.csv'
 
 #Specify output name
-OUTPUT = open('Edited_8100_ICUY_20160216_readable.csv', 'w+')
+OUTPUT = open('11Edited_8100_STAI_20160216_readable.csv', 'w+')
 
 
 # Read data file as csv
-DATACSV = pd.read_csv(DATA)
+#Doesn't import first row, needed for NKI, otherwise set skiprows=False
+#2nd row (or first remaining) row as column headers
+DATACSV = pd.read_csv(DATA, skiprows=0, header=1)
 
 
 # Make it a DataFrame
 df_DATACSV = pd.DataFrame(DATACSV)
+
+# Remove spaces from column names
+df_DATACSV.columns = df_DATACSV.columns.str.replace('\s+', '') 
+
+
+#Remove empty columns
+df_DATACSV = df_DATACSV.dropna(how='all', axis=1)  
+
 
 #Drop observations that contain a string: Basically create a new DataFrame
 #That doesn't contain raws wich had the value 'V1REP'
@@ -34,6 +45,9 @@ df_DATACSV[df_DATACSV.Visit != 'V1REP'] #Visit is the column (as python method),
 # Specify file path to save DataFrame as csv
 
 df_DATACSV.to_csv(OUTPUT)
+
+
+
 
 
 
